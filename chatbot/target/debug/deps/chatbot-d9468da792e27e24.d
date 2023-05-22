4c95fde5920521131067a050fb1d3f5cb476da30
JRosSx91 <hazee.1491@gmail.com>
2023-05-22 16:52:25 +0200

    adding dependencies

---
+++ chatbot/target/debug/deps/chatbot-d9468da792e27e24.d
@@ -0,0 +1,5 @@
+C:\Users\Ivan\labs\rust\Rust-Chatbot\chatbot\target\debug\deps\chatbot-d9468da792e27e24.rmeta: src\main.rs
+
+C:\Users\Ivan\labs\rust\Rust-Chatbot\chatbot\target\debug\deps\chatbot-d9468da792e27e24.d: src\main.rs
+
+src\main.rs:
---
+++ chatbot/target/debug/deps/dotenv-a625d7775b417781.d
@@ -0,0 +1,9 @@
+C:\Users\Ivan\labs\rust\Rust-Chatbot\chatbot\target\debug\deps\dotenv-a625d7775b417781.rmeta: C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\lib.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\parse.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\errors.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\iter.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\find.rs
+
+C:\Users\Ivan\labs\rust\Rust-Chatbot\chatbot\target\debug\deps\dotenv-a625d7775b417781.d: C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\lib.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\parse.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\errors.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\iter.rs C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\find.rs
+
+C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\lib.rs:
+C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\parse.rs:
+C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\errors.rs:
+C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\iter.rs:
+C:\Users\Ivan\.cargo\registry\src\github.com-1ecc6299db9ec823\dotenv-0.15.0\src\find.rs:
