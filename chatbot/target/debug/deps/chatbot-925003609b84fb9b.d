27239927d45433021f2e630190bcfa22bfa0c5fd
JRosSx91 <hazee.1491@gmail.com>
2023-05-22 16:19:41 +0200

    first print

---
+++ chatbot/target/debug/deps/chatbot-925003609b84fb9b.d
@@ -0,0 +1,5 @@
+C:\Users\Ivan\labs\rust\Rust-Chatbot\chatbot\target\debug\deps\chatbot-925003609b84fb9b.rmeta: src\main.rs
+
+C:\Users\Ivan\labs\rust\Rust-Chatbot\chatbot\target\debug\deps\chatbot-925003609b84fb9b.d: src\main.rs
+
+src\main.rs:
