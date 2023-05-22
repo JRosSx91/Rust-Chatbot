b10d0e48ca6e5c259c9e4188661954120611399b
JRosSx91 <hazee.1491@gmail.com>
2023-05-22 16:43:26 +0200

    creating async function to chatgpt

--- chatbot/src/main.rs
+++ chatbot/src/main.rs
@@ -1,21 +1,38 @@
+use reqwest;
+use serde_json::json;
+use std::env;
 use std::io::{self, Write};
 
-fn main() {
-    println!("Hello, I'm your ChatBot!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get the API key from an environment variable
+    let api_key = env::var("OPENAI_KEY").expect("OPENAI_KEY must be set");
 
-    loop {
-        print!("> ");
-        let mut stdout: io::Stdout = io::stdout();
-        stdout.flush().unwrap();
+    // Create a client
+    let client = reqwest::Client::new();
 
-        let mut input: String = String::new();
-        let stdin: io::Stdin = io::stdin();
-        stdin.read_line(&mut input).unwrap();
+    // Define the prompt and max tokens
+    let prompt = "Translate the following English text to French: '{}'";
+    let max_tokens = 60;
 
-        let input: &str = input.trim();
+    // Build the JSON body
+    let body = json!({
+        "prompt": prompt,
+        "max_tokens": max_tokens
+    });
 
-        if input.to_lowercase() == "exit" {
-            break;
-        }
-    }
+    // Send the request
+    let res = client
+        .post("https://api.openai.com/v1/engines/davinci-codex/completions")
+        .header("Authorization", format!("Bearer {}", api_key))
+        .json(&body)
+        .send()
+        .await?;
+
+    // Print the response status and text
+    println!("Status: {}", res.status());
+    let text: String = res.text().await?;
+    println!("Response:\n{}", text);
+
+    Ok(())
 }
