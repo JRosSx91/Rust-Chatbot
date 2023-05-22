42f4ec69486e9887a1061ee4f67e529d62453304
JRosSx91 <hazee.1491@gmail.com>
2023-05-22 16:24:03 +0200

    adding first functions

--- chatbot/src/main.rs
+++ chatbot/src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{self, Write};
+
 fn main() {
     println!("Hello, I'm your ChatBot!");
+
+    loop {
+        print!("> ");
+        let mut stdout: io::Stdout = io::stdout();
+        stdout.flush().unwrap();
+
+        let mut input: String = String::new();
+        let stdin: io::Stdin = io::stdin();
+        stdin.read_line(&mut input).unwrap();
+
+        let input: &str = input.trim();
+    }
 }
